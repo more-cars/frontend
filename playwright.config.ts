bb69d3d20ccd83c70dfc365f4585a8e56265cad5
Dennis Gerike <info+github@dennis-gerike.de>
2025-06-11 15:33:22 +0200

    changing the server port to avoid collisions with the API

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
 
     use: {
         // Base URL to use in actions like `await page.goto('/')`.
-        baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:3001',
 
         // Collect trace when retrying the failed test.
         trace: 'on-first-retry',
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import {app} from "./app"
 
-const PORT = 3000
+const PORT = 3001
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
