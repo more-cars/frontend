30707875e8e47ea75b72e5c46a49c556f84a0663
Dennis Gerike <info+github@dennis-gerike.de>
2025-06-12 17:05:47 +0200

    adding rudimentary accessibility test with "axe"

--- tests/e2e/brands.spec.ts
+++ tests/e2e/brands.spec.ts
@@ -1,4 +1,5 @@
 import {expect, test} from '@playwright/test'
+import AxeBuilder from "@axe-core/playwright"
 
 test('Brand overview page has a title', async ({page}) => {
     await page.goto('/brands')
@@ -12,4 +13,14 @@ test('Brand overview page has a list of all brands', async ({page}) => {
 
     await expect(page.getByRole('link'))
         .toHaveCount(3)
+})
+
+test('AXE', async ({page}) => {
+    await page.goto('/brands')
+
+    const accessibilityScanResults = await new AxeBuilder({page})
+        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+        .analyze()
+
+    expect(accessibilityScanResults.violations).toEqual([])
 })
