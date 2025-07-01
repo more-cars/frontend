3484fa6c4793e0190dca8194c040103c1068bb8d
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-01 11:01:53 +0200

    deploying dev container in minikube

---
+++ deployment/minikube-app-undeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# determining the path to this script,
+# to find the kubernetes scripts,
+# so this script can be called from any folder
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")/$1
+
+# making sure we are actually in the Minikube cluster, not in the Google cluster
+kubectl config use-context morecars
+
+# deleting the namespace, incl. all deployments, services and jobs
+kubectl delete namespace $1
