47e2d23f95aab2d1d60dd75a588c6abf012ab5f3
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-14 17:07:01 +0200

    fixing test

--- tests/a11y/brand.spec.ts
+++ tests/a11y/brand.spec.ts
@@ -2,7 +2,7 @@ import {test} from '@playwright/test'
 import {runAccessibilityScanner} from "./runAccessibilityScanner.js"
 
 test('Brands: Detail Page', async ({page}) => {
-    await page.goto('/brands/320')
+    await page.goto('/brands/573')
 
     await runAccessibilityScanner(page)
 })
