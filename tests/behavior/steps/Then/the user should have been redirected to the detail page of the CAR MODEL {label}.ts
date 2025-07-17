ed85566cc72600542e084f2b0b183a8f3d9b8b09
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-17 11:43:48 +0200

    MCF-96 implementing the Gherkin scenario "Navigating to a linked CAR MODEL"

---
+++ tests/behavior/steps/Then/the user should have been redirected to the detail page of the CAR MODEL {label}.ts
@@ -0,0 +1,12 @@
+import {createBdd} from "playwright-bdd"
+import {expect} from "@playwright/test"
+
+const {When} = createBdd()
+
+When('the user should have been redirected to the detail page of the CAR MODEL {string}',
+    async function ({page}, label: string) {
+        const carModelId: number = this.carModel
+
+        await expect(page.locator('[aria-label="id"]'))
+            .toHaveText(carModelId.toString())
+    })
