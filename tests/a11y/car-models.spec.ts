0b920f5cfda2389a15c06fdbeeb42ec70bc8f73c
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-17 16:22:23 +0200

    marking test as slow

--- tests/a11y/car-models.spec.ts
+++ tests/a11y/car-models.spec.ts
@@ -2,6 +2,7 @@ import {test} from '@playwright/test'
 import {runAccessibilityScanner} from './runAccessibilityScanner.ts'
 
 test('Car Models: Overview Page', async ({page}) => {
+    test.slow()
     await page.goto('/car-models')
 
     await runAccessibilityScanner(page)
