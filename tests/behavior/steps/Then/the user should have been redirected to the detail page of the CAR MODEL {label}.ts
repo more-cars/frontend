f9ca9ee795b83dfbfbee795763211692d014daa2
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-17 13:29:37 +0200

    quickfix

--- tests/behavior/steps/Then/the user should have been redirected to the detail page of the CAR MODEL {label}.ts
+++ tests/behavior/steps/Then/the user should have been redirected to the detail page of the CAR MODEL {label}.ts
@@ -1,9 +1,9 @@
 import {createBdd} from "playwright-bdd"
 import {expect} from "@playwright/test"
 
-const {When} = createBdd()
+const {Then} = createBdd()
 
-When('the user should have been redirected to the detail page of the CAR MODEL {string}',
+Then('the user should have been redirected to the detail page of the CAR MODEL {string}',
     async function ({page}, label: string) {
         const carModelId: number = this.carModel
 
