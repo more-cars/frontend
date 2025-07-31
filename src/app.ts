2d74d5f0ef2b9b381894c764348500114b469da5
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-31 13:38:33 +0200

    hiding the app behind a simple "basic authentication" as long as it is not live yet

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express from "express"
 import brands from './routes/brands.ts'
 import carModels from "./routes/car-models.ts"
 import images from "./routes/images.ts"
+import {basicAuthentication} from "./basicAuthentication.ts"
 
 const app: Express = express()
 app.set('view engine', 'pug')
 app.set('views', './src/views')
 
+app.use(basicAuthentication)
 app.use(express.static('public'))
 app.use('/', brands)
 app.use('/', carModels)
