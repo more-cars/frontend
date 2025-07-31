32a0bee8ea3a73359450d78b61cf1d77e85261d2
Dennis Gerike <info+github@dennis-gerike.de>
2025-07-31 11:15:54 +0200

    configuring the code coverage report for the "vitest" integration tests

--- tests/integration/vite.config.ts
+++ tests/integration/vite.config.ts
@@ -15,5 +15,23 @@ export default defineConfig(({mode}) => ({
         reporters: [
             'default',
         ],
+        coverage: {
+            provider: 'v8',
+            include: ['src/**'],
+            exclude: [
+                'src/**/types/**',
+                'src/server.ts',
+            ],
+            reporter: [
+                'text',
+                'html',
+            ],
+            reportsDirectory: 'test-reports/integration/coverage',
+            reportOnFailure: true,
+            thresholds: {
+                statements: 91,
+                branches: 66,
+            },
+        }
     },
 }))
