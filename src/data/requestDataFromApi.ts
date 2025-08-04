0e3805a341c51bb920db2b5d0c0248c4121bb2ea
Dennis Gerike <info+github@dennis-gerike.de>
2025-08-04 11:43:52 +0200

    adding authorization information to the API requests

--- src/data/requestDataFromApi.ts
+++ src/data/requestDataFromApi.ts
@@ -4,7 +4,12 @@ import {getApiBaseUrl} from "./getApiBaseUrl.ts"
 export async function requestDataFromApi(path: string) {
     try {
         const response = await axios
-            .get(`${getApiBaseUrl()}${path}`)
+            .get(`${getApiBaseUrl()}${path}`, {
+                auth: {
+                    username: process.env.API_BASIC_AUTH_USERNAME,
+                    password: process.env.API_BASIC_AUTH_PASSWORD,
+                }
+            })
         return response.data
     } catch (error) {
         console.error(`Error: ${error.code} - ${error.input} - ${path}`)
