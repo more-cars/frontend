4b3272838301b0a028149020067cebcf364b3b91
Dennis Gerike <info+github@dennis-gerike.de>
2025-08-15 14:36:00 +0200

    extending the specification importer to store a temporary copy of the fetched epics

---
+++ tests/sync-scripts/download-specification/cache-fetched-epics-from-jira.test.ts
@@ -0,0 +1,49 @@
+import fs from "fs"
+import {expect, test} from "vitest"
+import {cacheRawEpics} from "../../../specification/lib/cacheRawEpics.ts"
+
+test('Caching fetched epics at default path with default filename', async () => {
+    const fetchedEpics = JSON.parse(
+        fs.readFileSync(__dirname + '/../../_toolbox/fixtures/rawEpicsFromJira.json', {
+            encoding: 'utf8',
+            flag: 'r'
+        }))
+
+    cacheRawEpics(fetchedEpics)
+    expect(fs.existsSync('./_temp/epics.json'))
+        .toBeTruthy()
+
+    const cachedEpics = JSON.parse(fs.readFileSync('./_temp/epics.json', {
+        encoding: 'utf8',
+        flag: 'r'
+    }))
+    expect(cachedEpics)
+        .toEqual(fetchedEpics)
+
+    fs.rmSync('./_temp/epics.json')
+    expect(fs.existsSync('./_temp/epics.json'))
+        .toBeFalsy()
+})
+
+test('Caching fetched epics at custom path with custom filename', async () => {
+    const fetchedEpics = JSON.parse(
+        fs.readFileSync(__dirname + '/../../_toolbox/fixtures/rawEpicsFromJira.json', {
+            encoding: 'utf8',
+            flag: 'r'
+        }))
+
+    cacheRawEpics(fetchedEpics, 'testEpics.json', './_temporary')
+    expect(fs.existsSync('./_temporary/testEpics.json'))
+        .toBeTruthy()
+
+    const cachedEpics = JSON.parse(fs.readFileSync('./_temporary/testEpics.json', {
+        encoding: 'utf8',
+        flag: 'r'
+    }))
+    expect(cachedEpics)
+        .toEqual(fetchedEpics)
+
+    fs.rmSync('./_temporary', {recursive: true})
+    expect(fs.existsSync('./_temporary/testEpics.json'))
+        .toBeFalsy()
+})
