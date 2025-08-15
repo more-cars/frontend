b33a647b95dd7071f45298ab4840f78e2cacb429
Dennis Gerike <info+github@dennis-gerike.de>
2025-08-15 17:18:08 +0200

    adding jira link to epic markdown

--- specification/lib/createMarkdownForEpic.ts
+++ specification/lib/createMarkdownForEpic.ts
@@ -2,6 +2,7 @@ import type {Epic} from "./Types/Epic.ts"
 
 export function createMarkdownForEpic(epic: Epic) {
     let md = "# " + epic.title + "\n" + "\n"
+    md += "* Jira Link: https://dennis-gerike.atlassian.net/browse/" + epic.id + "\n"
     md += "* ID: " + epic.id + "\n"
     md += "* Created At: " + epic.created_at + "\n"
     md += "* Updated At: " + epic.updated_at + "\n"
--- tests/_toolbox/fixtures/epic.md
+++ tests/_toolbox/fixtures/epic.md
@@ -1,5 +1,6 @@
 # Node Type: Image
 
+* Jira Link: https://dennis-gerike.atlassian.net/browse/MCF-17
 * ID: MCF-17
 * Created At: 2025-06-10T19:13:15.921+0200
 * Updated At: 2025-08-13T17:42:17.175+0200
