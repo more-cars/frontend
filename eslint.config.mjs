ca7fc5f2c9f8e0e339aca3e3d3ff7eada0d05726
Dennis Gerike <info+github@dennis-gerike.de>
2025-11-05 11:39:11 +0100

    updating the typescript linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 
 import eslint from '@eslint/js'
+import {defineConfig} from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-duplicate-enum-values": "off"
+        }
+    }
 )
