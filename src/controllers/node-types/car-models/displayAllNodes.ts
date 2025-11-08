7b607f726bafd443c2e1b5fd60e3e4c9ed85ed2a
Dennis Gerike <info+github@dennis-gerike.de>
2025-11-08 14:26:02 +0100

    MCF-142 replacing the hard-coded primary properties

--- src/controllers/node-types/car-models/displayAllNodes.ts
+++ src/controllers/node-types/car-models/displayAllNodes.ts
@@ -6,7 +6,13 @@ export async function displayAllNodes(req: express.Request, res: express.Respons
 
     return res.render('templates/car-models/car-models-page', {
         pageTitle: 'All Car Models',
-        nodeCollection: carModels
+        nodeCollection: carModels,
+        primaryProperties: [
+            'built_from',
+            'built_to',
+            'generation',
+            'total_production',
+        ],
     }, (error, html) => {
         res.statusCode = 200
         res.send(html)
