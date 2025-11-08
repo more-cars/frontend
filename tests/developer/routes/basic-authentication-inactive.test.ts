ddf9080f295b088f729ce1efaad2c994f0b8bf99
Dennis Gerike <info+github@dennis-gerike.de>
2025-11-08 10:18:47 +0100

    MCF-127 enabling basic auth for the k8s environments

---
+++ tests/developer/routes/basic-authentication-inactive.test.ts
@@ -0,0 +1,37 @@
+import {afterAll, beforeAll, describe, expect, test} from 'vitest'
+import request from "supertest"
+import {app} from "../../../src/app"
+
+describe('Basic authentication is deactivated', () => {
+    beforeAll(async () => {
+        process.env.BASIC_AUTH_USERNAME = ''
+        process.env.BASIC_AUTH_PASSWORD = ''
+    })
+
+    afterAll(async () => {
+        delete process.env.BASIC_AUTH_USERNAME
+        delete process.env.BASIC_AUTH_PASSWORD
+    })
+
+    test('Request without credentials', async () => {
+        const response = await request(app)
+            .get('/brands')
+
+        expect(response.statusCode)
+            .toEqual(200)
+    })
+
+    test('Request with unnecessary credentials', async () => {
+        const username = 'username'
+        const password = 'password'
+
+        const token = Buffer.from(`${username}:${password}`).toString('base64')
+
+        const response = await request(app)
+            .get('/brands')
+            .set('Authorization', 'Basic ' + token)
+
+        expect(response.statusCode)
+            .toEqual(200)
+    })
+})
