838f6c3ea4f815038b9dd3d99df9ef14d96e25df
Dennis Gerike <info+github@dennis-gerike.de>
2025-11-08 15:20:32 +0100

    MCF-131 requesting the brand list sorted by name

--- src/data/node-types/brands/getAllBrands.ts
+++ src/data/node-types/brands/getAllBrands.ts
@@ -3,7 +3,7 @@ import type {ApiBrandNode} from "./types/ApiBrandNode"
 import type {BrandNode} from "./types/BrandNode"
 
 export async function getAllBrands() {
-    const apiData = (await requestDataFromApi('/brands')).data as ApiBrandNode[]
+    const apiData = (await requestDataFromApi('/brands?sort_by_property=name')).data as ApiBrandNode[]
     const data: BrandNode[] = []
 
     apiData.forEach(apiItem => {
