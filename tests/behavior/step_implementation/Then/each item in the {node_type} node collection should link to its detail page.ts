e7f4b5d06147cde0a70e406223bb9eb85837a673
Dennis Gerike <info+github@dennis-gerike.de>
2025-11-11 16:47:38 +0100

    MCF-173 updating the Gherkin scenario "Expecting a link to the detail page for each BRAND node in the list"

---
+++ tests/behavior/step_implementation/Then/each item in the {node_type} node collection should link to its detail page.ts
@@ -0,0 +1,11 @@
+import {Then} from "@badeball/cypress-cucumber-preprocessor"
+
+// TODO how to determine whether the correct page is linked?
+Then('each item in the {string} node collection should link to its detail page', (nodeType: string) => {
+    cy.get('[data-testid="brands-list"]')
+        .should('be.visible')
+
+    cy.get('[data-testid="brands-list"] li')
+        .find('a')
+        .should('be.visible')
+})
