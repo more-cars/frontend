78ec84ac0848a73d553c8f7799876c3961570879
Dennis Gerike <info+github@dennis-gerike.de>
2025-11-13 10:12:03 +0100

    telling axios to accept self-signed certificates (and not fail such requests)

--- src/data/requestDataFromApi.ts
+++ src/data/requestDataFromApi.ts
@@ -1,5 +1,12 @@
 import axios from "axios"
+import https from 'https'
 import {getApiBaseUrl} from "./getApiBaseUrl"
+
+// Axios will fail requests when the SSL certificate is invalid.
+// As long as the dev and testing environments have no valid certificates we have to tell axios to accept such requests.
+axios.defaults.httpsAgent = new https.Agent({
+    rejectUnauthorized: false
+})
 
 export async function requestDataFromApi(path: string) {
     try {
