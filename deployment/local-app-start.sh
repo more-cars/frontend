47eacac6939e4dd2d5b7219de70266639523a734
Dennis Gerike <dennis.gerike+github@gmail.com>
2025-12-21 17:28:40 +0100

    when starting the dev version of the app check that the hostname exists in the environment -> if not, add a warning to the console output

--- deployment/local-app-start.sh
+++ deployment/local-app-start.sh
@@ -25,6 +25,16 @@ if ! [ $? -eq 0 ]; then
   exit
 fi
 
+hostname_exists() {
+  if ! getent hosts $1 >/dev/null 2>&1; then
+      RED='\033[0;33m'
+      NC='\033[0m'
+      echo ⚠️ Hostname ${RED}$1${NC} was not found in the environment. Consider running ${RED}npm run local:hostnames:add${NC}.
+  fi
+}
+
+hostname_exists "frontend.more-cars.internal"
+
 echo 🟢 Starting tailwind watcher...
 
 npx @tailwindcss/cli -i src/views/stylesheets/main.css -o ./public/stylesheets/main.css --watch
