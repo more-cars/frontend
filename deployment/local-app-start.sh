a3e17ecc0f9a220bd572f157b021ecfffa447da8
Dennis Gerike <dennis.gerike+github@gmail.com>
2025-12-21 14:16:14 +0100

    when starting the dev version of the app check that the .env file exists -> if not, create it with sensible default values

--- deployment/local-app-start.sh
+++ deployment/local-app-start.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+create_env_file () {
+cat <<EOF >.env
+API_HOST=api.more-cars.internal
+API_PORT=3000
+EOF
+}
+
+if ! [ -f .env ]; then
+  echo "⚠️ .env file is missing"
+  echo "🪛 Creating a new .env file from scratch"
+  create_env_file
+  echo "✔️ .env file created"
+  echo
+fi
+
 echo 🟢 Starting application...
 
 node --watch --env-file=.env -r ts-node/register src/server.ts &
