963be355e0f3d56b1630a709bb9ad371cd87f937
Dennis Gerike <dennis.gerike+github@gmail.com>
2025-12-25 11:53:44 +0100

    creating patch file for ingress (so the correct URLs and hostnames are used for each environment and each cluster)

--- deployment/lib/create-patch-file.ts
+++ deployment/lib/create-patch-file.ts
@@ -1,13 +1,21 @@
 import fs from "node:fs"
+import {getHostname} from "./getHostname.ts"
 
-createPatchFile()
+createDeploymentPatchFile()
     .then((data) => {
         const path = __dirname + '/../app/'
-        const filename = 'patch.json'
+        const filename = 'deployment.patch.json'
         fs.writeFileSync(path + filename, JSON.stringify(data, null, 2))
     })
 
-async function createPatchFile() {
+createIngressPatchFile()
+    .then((data) => {
+        const path = __dirname + '/../app/'
+        const filename = 'ingress.patch.json'
+        fs.writeFileSync(path + filename, JSON.stringify(data, null, 2))
+    })
+
+async function createDeploymentPatchFile() {
     const packageName = process.env.PACKAGE_NAME
     const packageVersion = process.env.PACKAGE_VERSION
 
@@ -17,5 +25,23 @@ async function createPatchFile() {
             "path": "/spec/template/spec/containers/0/image",
             "value": `ghcr.io/more-cars/${packageName}:${packageVersion}`
         }
+    ]
+}
+
+async function createIngressPatchFile() {
+    const targetEnvironment = process.env.TARGET_ENVIRONMENT || 'prod'
+    const targetCluster = process.env.TARGET_CLUSTER || 'gke'
+
+    return [
+        {
+            "op": "replace",
+            "path": "/spec/rules/0/host",
+            "value": getHostname(targetCluster, targetEnvironment, 'frontend'),
+        },
+        {
+            "op": "replace",
+            "path": "/spec/tls/0/hosts/0",
+            "value": getHostname(targetCluster, targetEnvironment, 'frontend'),
+        },
     ]
 }
