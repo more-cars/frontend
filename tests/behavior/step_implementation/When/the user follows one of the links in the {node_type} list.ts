94519373ada9d2f4504f3cdce862a60f5deaf7df
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-04 16:10:02 +0100

    quickfix

--- tests/behavior/step_implementation/When/the user follows one of the links in the {node_type} list.ts
+++ tests/behavior/step_implementation/When/the user follows one of the links in the {node_type} list.ts
@@ -3,5 +3,6 @@ import {pluralize} from "inflection"
 
 When('the user follows one of the links in the {string} list', (nodeType: string) => {
     cy.get("section[aria-label='" + pluralize(nodeType.toLowerCase()) + "'] ul li a")
+        .first()
         .click()
 })
