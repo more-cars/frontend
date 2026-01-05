19053ddd77647ab57aa6e470a8da086aa269701f
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-05 13:15:26 +0100

    MCF-194 adding the brand's main image to the detail page

--- src/controllers/node-types/car-models/displayNode.ts
+++ src/controllers/node-types/car-models/displayNode.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import {CarModelModelFacade} from "../../../models/CarModelModelFacade"
 import {getPrimaryProperties} from "../../../models/node-types/getPrimaryProperties"
+import {BrandModelFacade} from "../../../models/BrandModelFacade"
 
 export async function displayNode(req: express.Request, res: express.Response) {
     const carModelId = parseInt(req.params.id)
@@ -17,12 +18,14 @@ export async function displayNode(req: express.Request, res: express.Response) {
 
     const connectedBrand = await CarModelModelFacade.getConnectedBrand(carModelId)
     const connectedImages = await CarModelModelFacade.getConnectedImages(carModelId)
+    const connectedMainImage = await BrandModelFacade.getConnectedMainImage(carModelId)
 
     res.render('templates/car-models/car-model-page', {
         pageTitle: `${carModel.name} - Car Model`,
         node: carModel,
         brand: connectedBrand,
         images: connectedImages,
+        mainImage: connectedMainImage,
         primaryProperties: getPrimaryProperties(),
     }, (error, html) => {
         res.statusCode = 200
