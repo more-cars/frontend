8b38566d76a3b57e842d11db580e93a914dfbc34
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-05 10:52:12 +0100

    using the actual property name instead of a placeholder and renaming the test step

--- tests/behavior/step_implementation/Then/the {node_type} list should be sorted by {prop} in {direction} order.ts
+++ tests/behavior/step_implementation/Then/the {node_type} list should be sorted by {prop} in {direction} order.ts
@@ -1,11 +1,11 @@
 import {Then} from "@badeball/cypress-cucumber-preprocessor"
 import {getNormalizedNodeType} from "../../lib/getNormalizedNodeType"
 
-Then('the items in the {string} list should be sorted by {string} in {string} order', (nodeType: string, property: string, sortDirection: string) => {
+Then('the {string} list should be sorted by {string} in {string} order', (nodeType: string, property: string, sortDirection: string) => {
     cy.get(`[data-testid="${getNormalizedNodeType(nodeType)}-list"]`)
         .should('be.visible')
 
-    cy.get(`[data-testid="${getNormalizedNodeType(nodeType)}-list"] > li [aria-label="name"]`)
+    cy.get(`[data-testid="${getNormalizedNodeType(nodeType)}-list"] > li [aria-label="${property}"]`)
         .should((items) => {
             const actualSorting = [...items].map(el => el.innerText.trim().toLowerCase())
             const expectedSorting = [...actualSorting].sort((a, b) => a.localeCompare(b))
