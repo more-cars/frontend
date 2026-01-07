e61ac21b1cdc0f861a2b3fe8741ee8c293b559d7
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-07 17:24:20 +0100

    quickfix

--- tests/behavior/step_implementation/When/the user follows the link in the predecessor section.ts
+++ tests/behavior/step_implementation/When/the user follows the link in the predecessor section.ts
@@ -1,6 +1,6 @@
 import {When} from "@badeball/cypress-cucumber-preprocessor"
 
 When('the user follows the link in the {string} predecessor section', (nodeType: string) => {
-    cy.get("section[aria-label='car-models-predecessor'] a")
+    cy.get("section[aria-label='car-model-predecessor'] a")
         .click()
 })
