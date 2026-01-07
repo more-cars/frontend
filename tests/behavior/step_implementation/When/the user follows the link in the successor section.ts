8b24d9fcc6a2546b9769ccd0009c768c132ff51f
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-07 16:43:42 +0100

    MCF-208 implementing the Gherkin scenario

---
+++ tests/behavior/step_implementation/When/the user follows the link in the successor section.ts
@@ -0,0 +1,6 @@
+import {When} from "@badeball/cypress-cucumber-preprocessor"
+
+When('the user follows the link in the {string} successor section', (nodeType: string) => {
+    cy.get("section[aria-label='car-models-successor'] a")
+        .click()
+})
