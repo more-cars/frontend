ff02720acdb9aa5c08df7d3b10d97d9cf0a57ac1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-07 12:06:40 +0100

    quickfix

--- src/data/node-types/car-models/types/CarModelHasSuccessorRelationship.ts
+++ src/data/node-types/car-models/types/CarModelHasSuccessorRelationship.ts
@@ -1,14 +1,13 @@
 import type {DataRelationshipType} from "../../../types/DataRelationshipType"
 import type {CarModelNode} from "./CarModelNode"
 import type {DataNodeType} from "../../../types/DataNodeType"
-import type {BrandNode} from "../../brands/types/BrandNode"
 
 export type CarModelHasSuccessorRelationship = {
     id: number
     name: DataRelationshipType.CAR_MODEL_HAS_SUCCESSOR
     source_node: CarModelNode
     source_node_type: DataNodeType.CAR_MODEL
-    partner_node: BrandNode
+    partner_node: CarModelNode
     partner_node_type: DataNodeType.CAR_MODEL
     created_at: string
     updated_at: string
