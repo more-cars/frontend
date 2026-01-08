bee2e323b3185558db9601ac6d36316e1e8609fd
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-08 11:53:42 +0100

    improving readability

--- tests/behavior/support-commands/basic-auth.ts
+++ tests/behavior/support-commands/basic-auth.ts
@@ -1,9 +1,12 @@
 Cypress.Commands.overwrite('visit', (original, url, options = {}) => {
-    return original(url, {
+    const extendedOptions = {
         ...options,
         auth: {
             username: Cypress.env('BASIC_AUTH_USERNAME'),
             password: Cypress.env('BASIC_AUTH_PASSWORD'),
         },
-    })
+    }
+
+    // @ts-ignore
+    return original(url, extendedOptions)
 })
