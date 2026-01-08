877e35bec926ebfba402916274bf0ac43ea2885b
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-08 11:58:50 +0100

    adding fallback for when "basic auth" credentials don't exist (Cypress tests will fail otherwise)

--- tests/behavior/support-commands/basic-auth.ts
+++ tests/behavior/support-commands/basic-auth.ts
@@ -2,8 +2,8 @@ Cypress.Commands.overwrite('visit', (original, url, options = {}) => {
     const extendedOptions = {
         ...options,
         auth: {
-            username: Cypress.env('BASIC_AUTH_USERNAME'),
-            password: Cypress.env('BASIC_AUTH_PASSWORD'),
+            username: Cypress.env('BASIC_AUTH_USERNAME') || '',
+            password: Cypress.env('BASIC_AUTH_PASSWORD') || '',
         },
     }
 
