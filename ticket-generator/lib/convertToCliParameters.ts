5475c35bc8cd88de43a307e8ad5fdb6163723ff6
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-10 11:00:29 +0100

    removing the ticket generator (this is the responsibility of a different repo now)

--- ticket-generator/lib/convertToCliParameters.ts
+++
@@ -1,9 +0,0 @@
-export function convertToCliParameters(parameters: any) {
-    const cliParams = []
-
-    for (const param in parameters) {
-        cliParams.push(`--${param}='${parameters[param]}'`)
-    }
-
-    return cliParams.join(' ')
-}
--- ticket-generator/lib/types/Story.ts
+++
@@ -1,7 +0,0 @@
-export type Story = {
-    title: string
-    userStory: string | null
-    specificationList: Array<string>
-    apiPath: string
-    responseOptions: string[]
-}
