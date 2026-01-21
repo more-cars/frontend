ea5162de3118feec062381c75d657d98f2439b99
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-21 18:39:45 +0100

    fixing "fact sheet" tests

--- tests/behavior/step_implementation/Then/the page should display a fact sheet.ts
+++ tests/behavior/step_implementation/Then/the page should display a fact sheet.ts
@@ -1,13 +1,13 @@
 import {Then} from "@badeball/cypress-cucumber-preprocessor"
 
 Then('the page should display a fact sheet', () => {
-    cy.get('[aria-label="fact sheet"]')
+    cy.get('[data-testid="fact-sheet"]')
         .should('be.visible')
 
-    cy.get('[aria-label="fact sheet"] h2')
+    cy.get('[data-testid="fact-sheet"] h2')
         .contains('Fact Sheet')
         .should('be.visible')
 
-    cy.get('[aria-label="fact sheet"] dl dt')
+    cy.get('[data-testid="fact-sheet"] tbody tr')
         .should('have.length.at.least', 1)
 })
