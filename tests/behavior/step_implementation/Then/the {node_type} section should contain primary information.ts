ed88c83c7379411d093ac18dc865fdd0d3edcb40
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-23 17:23:24 +0100

    fixing behavior test

--- tests/behavior/step_implementation/Then/the {node_type} section should contain primary information.ts
+++ tests/behavior/step_implementation/Then/the {node_type} section should contain primary information.ts
@@ -2,9 +2,9 @@ import {Then} from "@badeball/cypress-cucumber-preprocessor"
 import {getNormalizedNodeType} from "../../lib/getNormalizedNodeType"
 
 Then('the {string} section should contain primary information', (nodeType: string) => {
-    cy.get(`section[data-testid="${getNormalizedNodeType(nodeType)}-section"] [aria-label="Primary Information"]`)
+    cy.get(`section[data-testid="${getNormalizedNodeType(nodeType)}-section"] [data-testid="prime-data"]`)
         .should('be.visible')
 
-    cy.get(`section[data-testid="${getNormalizedNodeType(nodeType)}-section"] [aria-label="Primary Information"] li`)
+    cy.get(`section[data-testid="${getNormalizedNodeType(nodeType)}-section"] [data-testid="prime-data"] li`)
         .should('have.length.at.least', 1)
 })
