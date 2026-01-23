6b572f5e093abb3f755afaff4c8f6cb9675c5693
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-23 17:28:35 +0100

    fixing behavior tests

--- tests/behavior/step_implementation/Then/the page should contain a successor section.ts
+++ tests/behavior/step_implementation/Then/the page should contain a successor section.ts
@@ -1,6 +1,6 @@
 import {Then} from "@badeball/cypress-cucumber-preprocessor"
 
 Then('the page should contain a successor section', () => {
-    cy.get(`section[aria-label="car-model-successor"]`)
+    cy.get(`section[data-testid="successor-section"]`)
         .should('be.visible')
 })
--- tests/behavior/step_implementation/When/the user follows the link in the successor section.ts
+++ tests/behavior/step_implementation/When/the user follows the link in the successor section.ts
@@ -1,6 +1,6 @@
 import {When} from "@badeball/cypress-cucumber-preprocessor"
 
 When('the user follows the link in the {string} successor section', (nodeType: string) => {
-    cy.get("section[aria-label='car-model-successor'] a")
+    cy.get("section[data-testid='successor-section'] a")
         .click()
 })
