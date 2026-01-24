70b2bedfa2441d29e805636172531e048a604e25
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-24 18:33:47 +0100

    splitting the fact sheet into two parts -> node-specific data and More-Cars-internal data

--- src/models/node-types/getNodeProperties.ts
+++ src/models/node-types/getNodeProperties.ts
@@ -12,7 +12,34 @@ type NodeProperty = {
 }
 
 export function getNodeProperties(nodeType: DataNodeType) {
-    return JSON.parse(
+    const staticProperties: NodeProperty[] = [
+        {
+            name: "id",
+            label: "More Cars ID",
+            is_primary: false,
+            mandatory: true,
+            datatype: "number",
+            example: "12345678",
+        }, {
+            name: "created_at",
+            label: "Entry Created",
+            is_primary: false,
+            mandatory: true,
+            datatype: "string",
+            example: "2025-12-20",
+        }, {
+            name: "updated_at",
+            label: "Last Updated",
+            is_primary: false,
+            mandatory: true,
+            datatype: "string",
+            example: "2025-12-20",
+        }
+    ]
+
+    const nodeSpecificProperties = JSON.parse(
         readFileSync(`${__dirname}/../../data/node-types/${dasherize(pluralize(nodeType.toLowerCase()))}/properties.json`, 'utf-8')
     ) as NodeProperty[]
+
+    return [...staticProperties].concat(nodeSpecificProperties)
 }
