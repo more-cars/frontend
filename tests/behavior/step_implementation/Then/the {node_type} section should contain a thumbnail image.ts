1d319b9b26ff350f58222324830e02dee77580c3
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-25 16:09:24 +0100

    quickfix

--- tests/behavior/step_implementation/Then/the {node_type} section should contain a thumbnail image.ts
+++ tests/behavior/step_implementation/Then/the {node_type} section should contain a thumbnail image.ts
@@ -2,7 +2,7 @@ import {Then} from "@badeball/cypress-cucumber-preprocessor"
 import {getNormalizedNodeType} from "../../lib/getNormalizedNodeType"
 
 Then('the {string} section should contain a thumbnail image', (nodeType: string) => {
-    cy.get(`[data-testid="${getNormalizedNodeType(nodeType)}-section"] img`)
+    cy.get(`[data-testid="${getNormalizedNodeType(nodeType)}-section"]`)
         .find('img')
         .should('be.visible')
 })
