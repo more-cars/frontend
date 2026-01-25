23e8cb73c5396606aa6a11bb37772919c946d513
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-25 16:03:25 +0100

    removing obsolete test code

--- tests/behavior/step_implementation/Then/the page should contain a successor section.ts
+++
@@ -1,6 +0,0 @@
-import {Then} from "@badeball/cypress-cucumber-preprocessor"
-
-Then('the page should contain a successor section', () => {
-    cy.get(`section[data-testid="successor-section"]`)
-        .should('be.visible')
-})
--- tests/behavior/step_implementation/Then/the page should contain no successor section.ts
+++
@@ -1,6 +0,0 @@
-import {Then} from "@badeball/cypress-cucumber-preprocessor"
-
-Then('the page should contain no successor section', () => {
-    cy.get(`section[aria-label="car-model-successor"]`)
-        .should('not.exist')
-})
