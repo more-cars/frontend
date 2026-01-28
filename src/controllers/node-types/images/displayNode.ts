05508b448fa2b06ce60a4d26d24572c681f19524
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-01-28 10:24:10 +0100

    fixing the thumbnails of the relationships on the image detail page

--- src/controllers/node-types/images/displayNode.ts
+++ src/controllers/node-types/images/displayNode.ts
@@ -2,6 +2,8 @@ import express from "express"
 import {ImageModelFacade} from "../../../models/ImageModelFacade"
 import {getNodeProperties} from "../../../models/node-types/getNodeProperties"
 import {DataNodeType} from "../../../data/types/DataNodeType"
+import {getBrandThumbnails} from "../brands/getBrandThumbnails"
+import {getCarModelThumbnails} from "../car-models/getCarModelThumbnails"
 
 export async function displayNode(req: express.Request, res: express.Response) {
     const imageId = parseInt(req.params.id)
@@ -16,6 +18,9 @@ export async function displayNode(req: express.Request, res: express.Response) {
         })
     }
 
+    const brands = await ImageModelFacade.getConnectedBrands(imageId)
+    const carModels = await ImageModelFacade.getConnectedCarModels(imageId)
+
     res.render('templates/node-types/images/image-page', {
         page_title: `${image.name} - Image`,
         node: {
@@ -25,12 +30,14 @@ export async function displayNode(req: express.Request, res: express.Response) {
         },
         relationships: {
             brands: {
-                items: await ImageModelFacade.getConnectedBrands(imageId),
+                items: brands,
                 node_properties: getNodeProperties(DataNodeType.BRAND),
+                thumbnails: await getBrandThumbnails(brands),
             },
             car_models: {
-                items: await ImageModelFacade.getConnectedCarModels(imageId),
+                items: carModels,
                 node_properties: getNodeProperties(DataNodeType.CAR_MODEL),
+                thumbnails: await getCarModelThumbnails(carModels),
             },
         },
     })
