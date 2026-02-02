76fe843953cd333740242c422407edae5b6acfa8
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-02-02 15:43:16 +0100

    hardcoding the API protocol to HTTP (instead of HTTPS)

--- src/data/getApiBaseUrl.ts
+++ src/data/getApiBaseUrl.ts
@@ -9,7 +9,10 @@ export function getApiBaseUrl() {
         throw new Error('Missing hostname')
     }
 
-    const protocol = 'https'
+    // The frontend is assumed to always run in the same cluster as the API.
+    // Therefore, it can circumvent the gateway and talk directly with the API.
+    // Because the API only listens to HTTP requests, the protocol is hardcoded to HTTP here.
+    const protocol = 'http'
 
     return `${protocol}://${process.env.API_HOST}:${process.env.API_PORT}`
 }
