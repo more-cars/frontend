2e10054a181b043472b467983d236cbf75278ec2
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-08 12:04:46 +0100

    fixing test

--- tests/developer/controllers/start-page/collecting-node-type-information.test.ts
+++ tests/developer/controllers/start-page/collecting-node-type-information.test.ts
@@ -4,14 +4,13 @@ import {DataNodeType} from "../../../../src/data/types/DataNodeType"
 
 describe('Collecting Node Type information', () => {
     test('data is correctly assembled', async () => {
-        const nodeInformation = getNodeTypeInformation(DataNodeType.CAR_MODEL, '🗿', 1234)
+        const nodeInformation = getNodeTypeInformation(DataNodeType.CAR_MODEL, 1234)
 
         expect(nodeInformation)
             .toEqual({
                 name: 'car-model',
                 label: 'Car Models',
                 overview_page_path: '/car-models',
-                icon: '🗿',
                 count: 1234,
             })
     })
