2b46a6706ea0d345ec1f3504ff704c70254f561a
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-08 17:29:21 +0100

    fixing tests

--- tests/behavior/step_implementation/Then/the main headline should contain the {node_type} name.ts
+++ tests/behavior/step_implementation/Then/the main headline should contain the {node_type} name.ts
@@ -1,7 +1,12 @@
 import {Then} from "@badeball/cypress-cucumber-preprocessor"
 
 Then('the main headline should contain the {string} name', (nodeType: string) => {
-    cy.get('[data-testid="fact-sheet"] [data-testid="name"] td')
+    let targetProperty = 'name'
+    if (['lap time', 'session result'].includes(nodeType.toLowerCase())) {
+        targetProperty = 'driver_name'
+    }
+
+    cy.get(`[data-testid="fact-sheet"] [data-testid="${targetProperty}"] td`)
         .last()
         .invoke('text')
         .then((name: string) => {
