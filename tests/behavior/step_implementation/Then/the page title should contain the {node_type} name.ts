35e94a212b7e44c231992d05c082de088b20b1a5
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-10 16:32:02 +0100

    fixing behavior tests

--- tests/behavior/step_implementation/Then/the page title should contain the {node_type} name.ts
+++ tests/behavior/step_implementation/Then/the page title should contain the {node_type} name.ts
@@ -4,6 +4,8 @@ Then('the page title should contain the {string} name', (nodeType: string) => {
     let targetProperty = 'name'
     if (['lap time', 'session result'].includes(nodeType.toLowerCase())) {
         targetProperty = 'driver_name'
+    } else if (['magazine issue'].includes(nodeType.toLowerCase())) {
+        targetProperty = 'title'
     }
 
     cy.get(`[data-testid="fact-sheet"] [data-testid="${targetProperty}"] td`)
