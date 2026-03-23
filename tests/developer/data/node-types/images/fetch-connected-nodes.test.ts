f08bb12adc8be1902cd7b6f13d33ae754bb23888
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-23 11:00:28 +0100

    fixing tests

--- tests/developer/data/node-types/images/fetch-connected-nodes.test.ts
+++ tests/developer/data/node-types/images/fetch-connected-nodes.test.ts
@@ -19,9 +19,9 @@ describe('Fetching connected NODES from data source', () => {
         vi.doMock("../../../../../src/data/requestDataFromApi", () => ({
             requestDataFromApi: vi.fn(() => ({
                 data: [
-                    {data: {partner_node: {data: {id: 1}}, created_at: 'dummy', updated_at: 'dummy'}},
-                    {data: {partner_node: {data: {id: 2}}, created_at: 'dummy', updated_at: 'dummy'}},
-                    {data: {partner_node: {data: {id: 3}}, created_at: 'dummy', updated_at: 'dummy'}},
+                    {data: {partner_node: {node_type: 'brands', data: {id: 1}}, created_at: 'dummy', updated_at: 'dummy'}},
+                    {data: {partner_node: {node_type: 'brands', data: {id: 2}}, created_at: 'dummy', updated_at: 'dummy'}},
+                    {data: {partner_node: {node_type: 'brands', data: {id: 3}}, created_at: 'dummy', updated_at: 'dummy'}},
                 ]
             }))
         }))
