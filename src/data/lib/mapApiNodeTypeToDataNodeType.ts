95a3b4ecc81ec00fcfb04a5c26d0e0a9b71ef92c
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-23 10:04:19 +0100

    creating mapping between api node types and data node types

---
+++ src/data/lib/mapApiNodeTypeToDataNodeType.ts
@@ -0,0 +1,39 @@
+import {ApiNodeType} from "../types/ApiNodeType"
+import {DataNodeType} from "../types/DataNodeType"
+import {NodeTypeNotFoundError} from "../types/NodeTypeNotFoundError"
+
+export function mapApiNodeTypeToDataNodeType(apiNodeType: ApiNodeType): DataNodeType {
+    const mapping = new Map<ApiNodeType, DataNodeType>([
+        [ApiNodeType.COMPANY, DataNodeType.COMPANY],
+        [ApiNodeType.BRAND, DataNodeType.BRAND],
+        [ApiNodeType.CAR_MODEL, DataNodeType.CAR_MODEL],
+        [ApiNodeType.CAR_MODEL_VARIANT, DataNodeType.CAR_MODEL_VARIANT],
+        [ApiNodeType.PRICE, DataNodeType.PRICE],
+        [ApiNodeType.RACE_TRACK, DataNodeType.RACE_TRACK],
+        [ApiNodeType.TRACK_LAYOUT, DataNodeType.TRACK_LAYOUT],
+        [ApiNodeType.RACING_SERIES, DataNodeType.RACING_SERIES],
+        [ApiNodeType.RACING_EVENT, DataNodeType.RACING_EVENT],
+        [ApiNodeType.RACING_SESSION, DataNodeType.RACING_SESSION],
+        [ApiNodeType.SESSION_RESULT, DataNodeType.SESSION_RESULT],
+        [ApiNodeType.LAP_TIME, DataNodeType.LAP_TIME],
+        [ApiNodeType.RACING_GAME, DataNodeType.RACING_GAME],
+        [ApiNodeType.GAMING_PLATFORM, DataNodeType.GAMING_PLATFORM],
+        [ApiNodeType.MODEL_CAR, DataNodeType.MODEL_CAR],
+        [ApiNodeType.MODEL_CAR_BRAND, DataNodeType.MODEL_CAR_BRAND],
+        [ApiNodeType.MAGAZINE, DataNodeType.MAGAZINE],
+        [ApiNodeType.MAGAZINE_ISSUE, DataNodeType.MAGAZINE_ISSUE],
+        [ApiNodeType.RATING, DataNodeType.RATING],
+        [ApiNodeType.PROGRAMME, DataNodeType.PROGRAMME],
+        [ApiNodeType.PROGRAMME_EPISODE, DataNodeType.PROGRAMME_EPISODE],
+        [ApiNodeType.MOTOR_SHOW, DataNodeType.MOTOR_SHOW],
+        [ApiNodeType.IMAGE, DataNodeType.IMAGE],
+    ])
+
+    const mappedNodeType = mapping.get(apiNodeType)
+
+    if (mappedNodeType === null || mappedNodeType === undefined) {
+        throw new NodeTypeNotFoundError(apiNodeType)
+    }
+
+    return mappedNodeType
+}
