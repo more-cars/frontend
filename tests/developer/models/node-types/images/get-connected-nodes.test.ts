39b00cad6a2ebc6ce88da91a051a43e276e277b1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-23 10:55:48 +0100

    fixing tests

--- tests/developer/models/node-types/images/get-connected-nodes.test.ts
+++ tests/developer/models/node-types/images/get-connected-nodes.test.ts
@@ -5,6 +5,8 @@ import type {ImageBelongsToNodeRelationship} from "../../../../../src/data/node-
 import type {BrandNode} from "../../../../../src/data/node-types/brands/types/BrandNode"
 import type {CarModelNode} from "../../../../../src/data/node-types/car-models/types/CarModelNode"
 import {DataNodeType} from "../../../../../src/data/types/DataNodeType"
+import {DataRelationshipType} from "../../../../../src/data/types/DataRelationshipType"
+import type {ImageNode} from "../../../../../src/data/node-types/images/types/ImageNode"
 
 describe('Collect connected NDOES for the IMAGE detail page', () => {
     test('when no NODES are connected', async () => {
@@ -17,20 +19,28 @@ describe('Collect connected NDOES for the IMAGE detail page', () => {
     test('when there are NODES connected', async () => {
         vi.spyOn(ImageDataFacade, 'getConnectedNodes').mockResolvedValue([
             {
-                id: 2,
-                name: "dummy 1",
+                id: 41,
+                name: DataRelationshipType.IMAGE_BELONGS_TO_NODE,
+                source_node: {} as ImageNode,
+                source_node_type: DataNodeType.IMAGE,
                 partner_node: {} as BrandNode,
                 partner_node_type: DataNodeType.BRAND,
-            } as unknown as ImageBelongsToNodeRelationship,
+                created_at: "dummy",
+                updated_at: "dummy",
+            } as ImageBelongsToNodeRelationship,
             {
-                id: 2,
-                name: "dummy 1",
+                id: 42,
+                name: DataRelationshipType.IMAGE_BELONGS_TO_NODE,
+                source_node: {} as ImageNode,
+                source_node_type: DataNodeType.IMAGE,
                 partner_node: {} as CarModelNode,
                 partner_node_type: DataNodeType.CAR_MODEL,
-            } as unknown as ImageBelongsToNodeRelationship,
+                created_at: "dummy",
+                updated_at: "dummy",
+            } as ImageBelongsToNodeRelationship,
         ])
 
         expect(await findConnectedNodes(1))
-            .toHaveLength(1)
+            .toHaveLength(2)
     })
 })
