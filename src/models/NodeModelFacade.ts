93f1f2818f1bfa6fec92004d9f98c01e7ae03941
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-25 11:37:30 +0100

    removing obsolete function

--- src/models/NodeModelFacade.ts
+++ src/models/NodeModelFacade.ts
@@ -1,12 +1,7 @@
-import {findOutNodeTypeOfNode} from "./nodes/findOutNodeTypeOfNode"
 import {findNodeById} from "./nodes/findNodeById"
 
 export const NodeModelFacade = {
     async getNodeById(nodeId: number) {
         return findNodeById(nodeId)
-    },
-
-    async getNodeTypeOfNode(nodeId: number) {
-        return findOutNodeTypeOfNode(nodeId)
     },
 }
