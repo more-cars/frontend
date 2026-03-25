36a7e8b4cc4e97350038cf893e7b31c062db6ca8
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-25 10:21:30 +0100

    adding the option to redirect "node type" URLs to canonical URLs

--- src/controllers/RedirectControllerFacade.ts
+++ src/controllers/RedirectControllerFacade.ts
@@ -1,6 +1,7 @@
 import express, {type NextFunction} from "express"
 import {redirectLegacyUrlToCanonical} from "./redirects/redirectLegacyUrlToCanonical"
 import {redirectShortUrlToCanonical} from "./redirects/redirectShortUrlToCanonical"
+import {redirectNodeTypeUrlToCanonical} from "./redirects/redirectNodeTypeUrlToCanonical"
 
 export const RedirectControllerFacade = {
     async redirectLegacyUrl(req: express.Request, res: express.Response, next: NextFunction) {
@@ -9,5 +10,9 @@ export const RedirectControllerFacade = {
 
     async redirectShortUrl(req: express.Request, res: express.Response, next: NextFunction) {
         await redirectShortUrlToCanonical(req, res, next)
+    },
+
+    async redirectNodeTypeUrl(req: express.Request, res: express.Response) {
+        await redirectNodeTypeUrlToCanonical(req, res)
     },
 }
---
+++ src/controllers/redirects/redirectNodeTypeUrlToCanonical.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import {NodeModelFacade} from "../../models/NodeModelFacade"
+import {canonicalUrlPath} from "../../views/lib/canonicalUrlPath"
+import {getNodeTitle} from "../../models/getNodeTitle"
+
+export async function redirectNodeTypeUrlToCanonical(req: express.Request, res: express.Response) {
+    const nodeId = parseInt(req.params.id)
+    const modelNode = await NodeModelFacade.getNodeById(nodeId)
+
+    if (!modelNode) {
+        return res.render('templates/nodes/node-not-found-page', {
+            page_title: `Node not found`
+        }, (error, html) => {
+            res.statusCode = 404
+            res.send(html)
+        })
+    }
+
+    const redirectUrl = `../${canonicalUrlPath(nodeId, getNodeTitle(modelNode))}`
+
+    return res.redirect(301, redirectUrl)
+}
