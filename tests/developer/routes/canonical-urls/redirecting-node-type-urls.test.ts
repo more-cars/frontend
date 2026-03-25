f1abf8ba7d139d41d1592ceb9cf6b7d76e0d9925
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-25 11:10:58 +0100

    adding tests to check the redirecting of "node type" URLs

---
+++ tests/developer/routes/canonical-urls/redirecting-node-type-urls.test.ts
@@ -0,0 +1,33 @@
+import {describe, expect, test, vi} from "vitest"
+import {supertestGet} from "../../supertestGet"
+import {getAllExpectedNodeTypes} from "../../../_toolbox/getAllExpectedNodeTypes"
+import {NodeModelFacade} from "../../../../src/models/NodeModelFacade"
+import {convertStringToModelNodeType} from "../../../_toolbox/convertStringToNodeType"
+import type {ModelNode} from "../../../../src/models/types/ModelNode"
+import * as getNodeTitle from "../../../../src/models/getNodeTitle"
+
+describe('Redirecting node type URLs', () => {
+    test.each(
+        getAllExpectedNodeTypes()
+    )('for node type $0', async (nodeType) => {
+        vi.spyOn(NodeModelFacade, 'getNodeById')
+            .mockImplementation(async () => ({
+                type: convertStringToModelNodeType(nodeType),
+                fields: {},
+            } as ModelNode))
+
+        vi.spyOn(getNodeTitle, 'getNodeTitle')
+            .mockImplementation(() => ('Node Title'))
+
+        const response = await supertestGet('/car-models/1234')
+
+        expect(response.status)
+            .to.equal(301)
+
+        expect('location' in response.headers)
+            .toBeTruthy()
+
+        expect(response.headers['location'])
+            .toEqual('../node-title-1234')
+    })
+})
