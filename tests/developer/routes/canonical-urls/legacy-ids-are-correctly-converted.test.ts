1e40c8a61e88e3ea00dc21abb48ad900816fa4a1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-26 11:56:37 +0100

    cleanup

--- tests/developer/routes/canonical-urls/legacy-ids-are-correctly-converted.test.ts
+++ tests/developer/routes/canonical-urls/legacy-ids-are-correctly-converted.test.ts
@@ -1,27 +1,31 @@
 import {describe, expect, test} from "vitest"
 import {convertLegacyId} from "../../../../src/controllers/canonical/convertLegacyId"
 
-describe('Legacy IDs are correctly converted', () => {
-    test.each([
-        ['0', 10000000],
-        ['1', 10000001],
-        ['12', 10000012],
-        ['123', 10000123],
-        ['1234', 10001234],
-        ['12345', 10012345],
-        ['123456', 10123456],
-        ['999999', 10999999],
-    ])('Legacy: $0', async (legacyId, canonicalId) => {
-        expect(convertLegacyId(legacyId))
-            .to.equal(canonicalId)
+describe('Canonical IDs', () => {
+    describe('Legacy IDs are converted to new IDs', () => {
+        test.each([
+            ['0', 10000000],
+            ['1', 10000001],
+            ['12', 10000012],
+            ['123', 10000123],
+            ['1234', 10001234],
+            ['12345', 10012345],
+            ['123456', 10123456],
+            ['999999', 10999999],
+        ])('$0', async (legacyId, canonicalId) => {
+            expect(convertLegacyId(legacyId))
+                .to.equal(canonicalId)
+        })
     })
 
-    test.each([
-        ['10000000', 10000000],
-        ['12345678', 12345678],
-        ['99999999', 99999999],
-    ])('Modern: $0', async (legacyId, canonicalId) => {
-        expect(convertLegacyId(legacyId))
-            .to.equal(canonicalId)
+    describe('New IDs are NOT converted', () => {
+        test.each([
+            ['10000000', 10000000],
+            ['12345678', 12345678],
+            ['99999999', 99999999],
+        ])('$0', async (legacyId, canonicalId) => {
+            expect(convertLegacyId(legacyId))
+                .to.equal(canonicalId)
+        })
     })
 })
