3c21e731ada46f2d23ee8f283d57f0c04fb2af03
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-27 11:26:50 +0100

    fixing node type converter

--- tests/_toolbox/convertStringToNodeType.ts
+++ tests/_toolbox/convertStringToNodeType.ts
@@ -1,5 +1,5 @@
 import {humanize, pluralize, underscore} from "inflection"
-import {kebabCase} from "change-case"
+import {kebabCase, pascalCase} from "change-case"
 import type {ApiNodeType} from "../../src/data/types/ApiNodeType"
 import type {ModelNodeType} from "../../src/models/types/ModelNodeType"
 import type {DataNodeType} from "../../src/data/types/DataNodeType"
@@ -9,7 +9,7 @@ export function convertStringToApiNodeType(rawNodeType: string) {
 }
 
 export function convertStringToModelNodeType(rawNodeType: string) {
-    return underscore(kebabCase(rawNodeType.toLowerCase())) as ModelNodeType
+    return underscore(pascalCase(rawNodeType.toLowerCase())) as ModelNodeType
 }
 
 export function convertStringToDataNodeType(rawNodeType: string) {
