d7a949f6c8ee04eafded17e1ac9c6c2086545de2
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-03-27 11:28:09 +0100

    adding tests for the generated "node titles"

---
+++ tests/developer/models/nodes/node-title.test.ts
@@ -0,0 +1,29 @@
+import {describe, expect, test} from "vitest"
+import {getAllExpectedNodeTypes} from "../../../_toolbox/getAllExpectedNodeTypes"
+import {convertStringToModelNodeType} from "../../../_toolbox/convertStringToNodeType"
+import {getNodeTitle} from "../../../../src/models/getNodeTitle"
+import type {ModelNode} from "../../../../src/models/types/ModelNode"
+
+describe('Node Title', () => {
+    test.each(
+        getAllExpectedNodeTypes()
+    )('$0', async (nodeType) => {
+        const data = {
+            type: convertStringToModelNodeType(nodeType),
+            fields: {
+                name: 'dummy',
+                title: 'dummy',
+                driver_name: 'dummy',
+                position: 1,
+            }
+        }
+
+        const nodeTitle = getNodeTitle(data as unknown as ModelNode)
+
+        expect(typeof nodeTitle)
+            .toBe('string')
+
+        expect(nodeTitle)
+            .not.toBe('')
+    })
+})
