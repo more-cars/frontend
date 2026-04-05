c100e5c499a8f719728cf99748a529dd7eb16fe7
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-05 17:36:25 +0200

    fixing tests

--- tests/behavior/step_implementation/Then/the main headline should contain the {node_type} name.ts
+++ tests/behavior/step_implementation/Then/the main headline should contain the {node_type} name.ts
@@ -4,7 +4,7 @@ Then('the main headline should contain the {string} name', (nodeType: string) =>
     let targetProperty = 'name'
     if (['lap time', 'session result'].includes(nodeType.toLowerCase())) {
         targetProperty = 'driver_name'
-    } else if (['magazine issue', 'programme episode'].includes(nodeType.toLowerCase())) {
+    } else if (['magazine issue', 'programme episode', 'video'].includes(nodeType.toLowerCase())) {
         targetProperty = 'title'
     } else if (['rating'].includes(nodeType.toLowerCase())) {
         targetProperty = 'rating_value'
