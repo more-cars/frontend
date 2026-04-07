6fae85fd56939a8bd115b6ae331f1143c6e52dac
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-07 17:15:39 +0200

    MCF-2139 implementing the Gherkin scenario

--- tests/behavior/step_implementation/Then/each item in the {node_type} list should link to its detail page.ts
+++ tests/behavior/step_implementation/Then/each item in the {node_type} list should link to its detail page.ts
@@ -2,7 +2,7 @@ import {Then} from "@badeball/cypress-cucumber-preprocessor"
 import {getNormalizedNodeType} from "../../lib/getNormalizedNodeType"
 
 // TODO how to determine whether the correct page is linked?
-Then('each item in the {string} list should link to its detail page', (nodeType: string) => {
+Then('each item in the {string} list/section should link to its detail page', (nodeType: string) => {
     cy.get(`section[data-testid="${getNormalizedNodeType(nodeType)}-section"]`)
         .should('be.visible')
 
