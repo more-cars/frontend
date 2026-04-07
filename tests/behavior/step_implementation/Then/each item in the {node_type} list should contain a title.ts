7ab583b38279bb9f01db37085cde1c99d45ae7fe
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-07 17:03:02 +0200

    MCF-2140 implementing the Gherkin scenario

--- tests/behavior/step_implementation/Then/each item in the {node_type} list should contain a title.ts
+++ tests/behavior/step_implementation/Then/each item in the {node_type} list should contain a title.ts
@@ -1,10 +1,10 @@
 import {Then} from "@badeball/cypress-cucumber-preprocessor"
 import {getNormalizedNodeType} from "../../lib/getNormalizedNodeType"
 
-Then('each item in the {string} list/section should contain a thumbnail image', (nodeType: string) => {
+Then('each item in the {string} list/section should contain a title', (nodeType: string) => {
     cy.get(`[data-testid="${getNormalizedNodeType(nodeType)}-section"] > ul > li`).then(items => {
         cy.wrap(items)
-            .find('img')
-            .should('have.length', items.length)
+            .find('a span')
+            .should('not.be.empty')
     })
 })
