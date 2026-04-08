55b481f355b479e7c3d711f2eeef0f569260b9b7
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-08 19:41:08 +0200

    fixing pagination bar code and tests

--- tests/behavior/step_implementation/Then/the pagination bar should not be visible.ts
+++ tests/behavior/step_implementation/Then/the pagination bar should not be visible.ts
@@ -1,6 +1,16 @@
 import {Then} from "@badeball/cypress-cucumber-preprocessor"
 
+// TODO clarify specification
 Then('the pagination bar should not be visible', () => {
-    cy.get('[aria-label="pagination"]')
+    cy.get('[aria-label="pagination"] [aria-label="first page"]')
+        .should('not.exist')
+
+    cy.get('[aria-label="pagination"] [aria-label="last page"]')
+        .should('not.exist')
+
+    cy.get('[aria-label="pagination"] [aria-label="previous page"]')
+        .should('not.exist')
+
+    cy.get('[aria-label="pagination"] [aria-label="next page"]')
         .should('not.exist')
 })
