f591ad0f4f5124c721d56b10d832547cb42349b7
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 19:52:13 +0200

    updating subtitle for node type IMAGE

--- src/models/node-types/images/getNodeSubTitle.ts
+++ src/models/node-types/images/getNodeSubTitle.ts
@@ -1,5 +1,23 @@
 import type {Image} from "./types/Image"
 
 export function getNodeSubTitle(node: Image) {
-    return `${node.fields.license} | ${node.fields.creator} | via ${node.fields.image_provider}`
+    const license = node.fields.license
+    const creator = node.fields.creator
+    const provider = node.fields.image_provider
+
+    const subtitleParts = []
+
+    if (creator) {
+        subtitleParts.push(`by ${creator}`)
+    }
+
+    if (provider) {
+        subtitleParts.push(`via ${provider}`)
+    }
+
+    if (license) {
+        subtitleParts.push(`license: ${license}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
