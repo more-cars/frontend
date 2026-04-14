0bcbad986a2d35c1beb9dc01bf8a8b83c29388eb
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 19:27:53 +0200

    updating subtitle for node type PROGRAMME

--- src/models/node-types/programmes/getNodeSubTitle.ts
+++ src/models/node-types/programmes/getNodeSubTitle.ts
@@ -1,5 +1,22 @@
 import type {Programme} from "./types/Programme"
 
 export function getNodeSubTitle(node: Programme) {
-    return `${node.fields.aired_from_year} - ${node.fields.aired_until_year}`
+    const from = node.fields.aired_from_year
+    const to = node.fields.aired_until_year
+
+    const subtitleParts = []
+
+    if (from && to) {
+        subtitleParts.push(`${from} - ${to}`)
+    }
+
+    if (from && !to) {
+        subtitleParts.push(`since ${from}`)
+    }
+
+    if (!from && to) {
+        subtitleParts.push(`until ${to}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
