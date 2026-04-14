456a1737a32b7651d2ffe9574f7eb265b2ae878f
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 16:27:00 +0200

    updating subtitle for node type CAR MODEL VARIANT

--- src/models/node-types/car-model-variants/getNodeSubTitle.ts
+++ src/models/node-types/car-model-variants/getNodeSubTitle.ts
@@ -1,5 +1,51 @@
 import type {CarModelVariant} from "./types/CarModelVariant"
 
 export function getNodeSubTitle(node: CarModelVariant) {
-    return `${node.fields.built_from} - ${node.fields.built_to} | ${node.fields.internal_code} | ${node.fields.transmission}`
+    const builtFrom = node.fields.built_from
+    const builtTo = node.fields.built_to
+    const internalCode = node.fields.internal_code
+    const transmission = node.fields.transmission
+
+    const subtitle = []
+
+    if (builtFrom && builtTo) {
+        subtitle.push(`${builtFrom} - ${builtTo}`)
+    }
+
+    if (builtFrom && !builtTo) {
+        subtitle.push(`since ${builtFrom}`)
+    }
+
+    if (!builtFrom && builtTo) {
+        subtitle.push(`until ${builtTo}`)
+    }
+
+    if (internalCode) {
+        subtitle.push(internalCode)
+    }
+
+    if (transmission) {
+        subtitle.push(getFormattedTransmission(transmission))
+    }
+
+    return subtitle.join(' | ')
+}
+
+function getFormattedTransmission(transmission: string) {
+    switch (transmission.toLowerCase()) {
+        case 'auto':
+            return 'Automatic'
+        case 'manual':
+            return 'Manual'
+        case 'dual clutch':
+            return 'Dual Clutch'
+        case 'cvt':
+            return 'CVT'
+        case 'sequential':
+            return 'Sequential'
+        case 'automated manual':
+            return 'Automated Manual'
+    }
+
+    return transmission
 }
