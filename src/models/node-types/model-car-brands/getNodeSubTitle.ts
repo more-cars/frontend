036d54df0ee76e52c9d8c491417ab52505bf254e
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 20:15:34 +0200

    updating subtitle for node type MODEL CAR BRAND

--- src/models/node-types/model-car-brands/getNodeSubTitle.ts
+++ src/models/node-types/model-car-brands/getNodeSubTitle.ts
@@ -1,5 +1,22 @@
 import type {ModelCarBrand} from "./types/ModelCarBrand"
 
 export function getNodeSubTitle(node: ModelCarBrand) {
-    return `${node.fields.founded} - ${node.fields.defunct}`
+    const founded = node.fields.founded
+    const defunct = node.fields.defunct
+
+    const subtitleParts = []
+
+    if (founded && defunct) {
+        subtitleParts.push(`${founded} - ${defunct}`)
+    }
+
+    if (founded && !defunct) {
+        subtitleParts.push(`since ${founded}`)
+    }
+
+    if (!founded && defunct) {
+        subtitleParts.push(`until ${defunct}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
