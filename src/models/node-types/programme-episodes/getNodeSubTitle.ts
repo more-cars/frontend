5328f2efa04d8302b24f35085d9c3dfeef3c344f
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 19:35:34 +0200

    updating subtitle for node type PROGRAMME EPISODE

--- src/models/node-types/programme-episodes/getNodeSubTitle.ts
+++ src/models/node-types/programme-episodes/getNodeSubTitle.ts
@@ -1,5 +1,24 @@
 import type {ProgrammeEpisode} from "./types/ProgrammeEpisode"
+import {convertDate} from "../../../views/lib/convertDate"
 
 export function getNodeSubTitle(node: ProgrammeEpisode) {
-    return `${node.fields.original_air_date} | Season ${node.fields.season_number} | Episode ${node.fields.season_episode_number}`
+    const date = node.fields.original_air_date
+    const season = node.fields.season_number
+    const episode = node.fields.season_episode_number
+
+    const subtitleParts = []
+
+    if (date) {
+        subtitleParts.push(convertDate(date))
+    }
+
+    if (season) {
+        subtitleParts.push(`Season ${season}`)
+    }
+
+    if (episode) {
+        subtitleParts.push(`Episode ${episode}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
