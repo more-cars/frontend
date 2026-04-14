f34ee6c6213b86a82b285ce332afd2b534470162
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 20:13:55 +0200

    updating subtitle for node type MODEL CAR

--- src/models/node-types/model-cars/getNodeSubTitle.ts
+++ src/models/node-types/model-cars/getNodeSubTitle.ts
@@ -1,5 +1,18 @@
 import type {ModelCar} from "./types/ModelCar"
 
 export function getNodeSubTitle(node: ModelCar) {
-    return `${node.fields.scale} | ${node.fields.release_year}`
+    const scale = node.fields.scale
+    const year = node.fields.release_year
+
+    const subtitleParts = []
+
+    if (scale) {
+        subtitleParts.push(scale)
+    }
+
+    if (year) {
+        subtitleParts.push(year)
+    }
+
+    return subtitleParts.join(' | ')
 }
