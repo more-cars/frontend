fc93c4ffcb94dec48270c85f0f12a5caac38086e
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 17:17:16 +0200

    updating subtitle for node type RACING SERIES

--- src/models/node-types/racing-series/getNodeSubTitle.ts
+++ src/models/node-types/racing-series/getNodeSubTitle.ts
@@ -1,5 +1,27 @@
 import type {RacingSeries} from "./types/RacingSeries"
 
 export function getNodeSubTitle(node: RacingSeries) {
-    return `${node.fields.short_name} | ${node.fields.founded} - ${node.fields.defunct}`
+    const shortName = node.fields.short_name
+    const founded = node.fields.founded
+    const defunct = node.fields.defunct
+
+    const subtitleParts = []
+
+    if (shortName) {
+        subtitleParts.push(shortName)
+    }
+
+    if (founded && defunct) {
+        subtitleParts.push(`${founded} - ${defunct}`)
+    }
+
+    if (founded && !defunct) {
+        subtitleParts.push(`since ${founded}`)
+    }
+
+    if (!founded && defunct) {
+        subtitleParts.push(`until ${defunct}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
