8af47e386a20dcee916aa65c893b9f0a79a60858
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 19:44:15 +0200

    updating subtitle for node type MOTOR SHOW

--- src/models/node-types/motor-shows/getNodeSubTitle.ts
+++ src/models/node-types/motor-shows/getNodeSubTitle.ts
@@ -2,5 +2,22 @@ import type {MotorShow} from "./types/MotorShow"
 import {convertDate} from "../../../views/lib/convertDate"
 
 export function getNodeSubTitle(node: MotorShow) {
-    return `${node.fields.date_from ? convertDate(node.fields.date_from) : '?'} until ${node.fields.date_until ? convertDate(node.fields.date_until) : '?'}`
+    const from = node.fields.date_from
+    const until = node.fields.date_until
+
+    const subtitleParts = []
+
+    if (from && until) {
+        subtitleParts.push(`${convertDate(from)} - ${convertDate(until)}`)
+    }
+
+    if (from && !until) {
+        subtitleParts.push(`from ${convertDate(from)}`)
+    }
+
+    if (!from && until) {
+        subtitleParts.push(`until ${convertDate(until)}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
