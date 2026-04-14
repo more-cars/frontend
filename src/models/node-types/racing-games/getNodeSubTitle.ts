69fc8d0b42854fdd04dec2d36fe9805b02b9f46a
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 20:10:07 +0200

    updating subtitle for node type RACING GAME

--- src/models/node-types/racing-games/getNodeSubTitle.ts
+++ src/models/node-types/racing-games/getNodeSubTitle.ts
@@ -1,5 +1,18 @@
 import type {RacingGame} from "./types/RacingGame"
 
 export function getNodeSubTitle(node: RacingGame) {
-    return `${node.fields.release_year} | ${node.fields.developer}`
+    const year = node.fields.release_year
+    const developer = node.fields.developer
+
+    const subtitleParts = []
+
+    if (year) {
+        subtitleParts.push(year)
+    }
+
+    if (developer) {
+        subtitleParts.push(developer)
+    }
+
+    return subtitleParts.join(' | ')
 }
