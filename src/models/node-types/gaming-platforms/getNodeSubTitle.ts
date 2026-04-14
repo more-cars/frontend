49b1a83991542f57577469f7d7f8578e8baab508
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 20:11:56 +0200

    updating subtitle for node type GAMING PLATFORM

--- src/models/node-types/gaming-platforms/getNodeSubTitle.ts
+++ src/models/node-types/gaming-platforms/getNodeSubTitle.ts
@@ -1,5 +1,18 @@
 import type {GamingPlatform} from "./types/GamingPlatform"
 
 export function getNodeSubTitle(node: GamingPlatform) {
-    return `${node.fields.release_year} | ${node.fields.manufacturer}`
+    const year = node.fields.release_year
+    const manufacturer = node.fields.manufacturer
+
+    const subtitleParts = []
+
+    if (year) {
+        subtitleParts.push(year)
+    }
+
+    if (manufacturer) {
+        subtitleParts.push(manufacturer)
+    }
+
+    return subtitleParts.join(' | ')
 }
