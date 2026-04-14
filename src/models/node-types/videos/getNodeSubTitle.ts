750e9878bf812544d49ad9b2194c9c4133caadf5
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 20:00:34 +0200

    updating subtitle for node type VIDEO

--- src/models/node-types/videos/getNodeSubTitle.ts
+++ src/models/node-types/videos/getNodeSubTitle.ts
@@ -1,5 +1,28 @@
 import type {Video} from "./types/Video"
+import {formatTimeShort} from "../../../views/lib/formatTime"
 
 export function getNodeSubTitle(node: Video) {
-    return `${node.fields.creator} | ${node.fields.duration}`
+    const creator = node.fields.creator
+    const provider = node.fields.video_provider
+    const duration = node.fields.duration
+
+    const subtitleParts = []
+
+    if (creator) {
+        subtitleParts.push(`by ${creator}`)
+    }
+
+    if (provider) {
+        if (provider === 'youtube') {
+            subtitleParts.push(`via YouTube`)
+        } else {
+            subtitleParts.push(`via ${provider}`)
+        }
+    }
+
+    if (duration) {
+        subtitleParts.push(formatTimeShort(duration))
+    }
+
+    return subtitleParts.join(' | ')
 }
