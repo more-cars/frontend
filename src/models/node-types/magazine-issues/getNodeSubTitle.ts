00edf8d40eedb38cbfa094cb99ffc5a614dd56f1
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 19:20:30 +0200

    updating subtitle for node type MAGAZINE ISSUE

--- src/models/node-types/magazine-issues/getNodeSubTitle.ts
+++ src/models/node-types/magazine-issues/getNodeSubTitle.ts
@@ -1,5 +1,23 @@
 import type {MagazineIssue} from "./types/MagazineIssue"
 
 export function getNodeSubTitle(node: MagazineIssue) {
-    return `Issue ${node.fields.issue_number}/${node.fields.issue_year} | ${node.fields.pages} pages`
+    const issue = node.fields.issue_number
+    const year = node.fields.issue_year
+    const pages = node.fields.pages
+
+    const subtitleParts = []
+
+    if (issue && year) {
+        subtitleParts.push(`Issue ${issue}/${year}`)
+    }
+
+    if (!issue && year) {
+        subtitleParts.push(year)
+    }
+
+    if (pages) {
+        subtitleParts.push(`${node.fields.pages} pages`)
+    }
+
+    return subtitleParts.join(' | ')
 }
