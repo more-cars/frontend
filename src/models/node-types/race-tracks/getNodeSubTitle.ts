65ab0fb8c7e7bf1ca6f0f2ce846a6c11d07f95bd
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 18:47:18 +0200

    updating subtitle for node type RACE TRACK

--- src/models/node-types/race-tracks/getNodeSubTitle.ts
+++ src/models/node-types/race-tracks/getNodeSubTitle.ts
@@ -1,5 +1,27 @@
 import type {RaceTrack} from "./types/RaceTrack"
 
 export function getNodeSubTitle(node: RaceTrack) {
-    return `${node.fields.opened} - ${node.fields.closed} | ${node.fields.location}`
+    const opened = node.fields.opened
+    const closed = node.fields.closed
+    const location = node.fields.location
+
+    const subtitleParts = []
+
+    if (opened && closed) {
+        subtitleParts.push(`${node.fields.opened} - ${node.fields.closed}`)
+    }
+
+    if (opened && !closed) {
+        subtitleParts.push(`since ${node.fields.opened}`)
+    }
+
+    if (!opened && closed) {
+        subtitleParts.push(`until ${node.fields.closed}`)
+    }
+
+    if (location) {
+        subtitleParts.push(node.fields.location)
+    }
+
+    return subtitleParts.join(' | ')
 }
