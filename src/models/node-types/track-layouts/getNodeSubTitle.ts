cf94190197070e2715664322cfd4c0b885bd4948
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 18:53:47 +0200

    updating subtitle for node type TRACK LAYOUT

--- src/models/node-types/track-layouts/getNodeSubTitle.ts
+++ src/models/node-types/track-layouts/getNodeSubTitle.ts
@@ -1,5 +1,28 @@
 import type {TrackLayout} from "./types/TrackLayout"
 
 export function getNodeSubTitle(node: TrackLayout) {
-    return `${node.fields.length} ${node.fields.length_unit} | ${node.fields.year_from} - ${node.fields.year_to}`
+    const year_from = node.fields.year_from
+    const year_to = node.fields.year_to
+    const length = node.fields.length
+    const lengthUnit = node.fields.length_unit
+
+    const subtitleParts = []
+
+    if (year_from && year_to) {
+        subtitleParts.push(`${node.fields.year_from} - ${node.fields.year_to}`)
+    }
+
+    if (year_from && !year_to) {
+        subtitleParts.push(`since ${node.fields.year_from}`)
+    }
+
+    if (!year_from && year_to) {
+        subtitleParts.push(`until ${node.fields.year_to}`)
+    }
+
+    if (length) {
+        subtitleParts.push(`${length} ${lengthUnit}`)
+    }
+
+    return subtitleParts.join(' | ')
 }
