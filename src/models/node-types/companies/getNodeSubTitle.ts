25dedf802cadbc0d7f6af9970366b4d22622a3dd
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-14 15:52:44 +0200

    updating subtitle for node type COMPANY

--- src/models/node-types/companies/getNodeSubTitle.ts
+++ src/models/node-types/companies/getNodeSubTitle.ts
@@ -1,5 +1,20 @@
 import type {Company} from "./types/Company"
 
 export function getNodeSubTitle(node: Company) {
-    return `${node.fields.founded} - ${node.fields.defunct}`
+    const founded = node.fields.founded
+    const defunct = node.fields.defunct
+
+    if (founded && defunct) {
+        return `${founded} - ${defunct}`
+    }
+
+    if (founded && !defunct) {
+        return `since ${founded}`
+    }
+
+    if (!founded && defunct) {
+        return `until ${defunct}`
+    }
+
+    return ''
 }
