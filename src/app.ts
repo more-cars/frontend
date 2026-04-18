3f2a455e019713b48acabda329fce62c5d2eb31c
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-04-18 19:48:55 +0200

    activating gzip

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import type {Express} from "express"
 import express from "express"
+import compression from 'compression'
 import canonicalAndRedirectUrls from "./routes/canonical-and-redirect-urls"
 import startPage from "./routes/startPage.ts"
 import {analyticsMiddleware} from "./tracking/analyticsMiddleware"
@@ -7,13 +7,16 @@ import {basicAuthentication} from "./basicAuthentication"
 import {registerViewHelpers} from "./routes/registerViewHelpers"
 import {registerNodeTypeRoutes} from "./routes/registerNodeTypeRoutes"
 
-const app: Express = express()
+const app = express()
+
 app.set('view engine', 'pug')
 app.set('views', './src/views')
 app.set('trust proxy', true) // needed, so the tracking still works with Kubernetes+Gateway+HTTPRoute
 
+app.use(compression())
 app.use(analyticsMiddleware)
 app.use(basicAuthentication)
+
 registerViewHelpers(app)
 
 app.use(express.static('public'))
