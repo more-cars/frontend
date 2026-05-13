a1f118fd76292bfe8a3eb3ac20c8d2c2ffbb7071
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-05-13 19:20:49 +0200

    setting content security policy header for most of the pages

--- src/controllers/responses/sendResponse200.ts
+++ src/controllers/responses/sendResponse200.ts
@@ -2,6 +2,7 @@ import express from "express"
 
 export function sendResponse200(data: unknown, res: express.Response) {
     res.set('Content-Type', 'text/html; charset=utf-8')
+    res.set('Content-Security-Policy', "default-src 'self'; img-src 'self' data: upload.wikimedia.org live.staticflickr.com i.ytimg.com; frame-src www.youtube-nocookie.com")
     res.set('Cache-Control', 'public, max-age=600, stale-while-revalidate=60')
     res.send(data)
 }
