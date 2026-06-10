e88154d1582ec73bc7c8c1dcd9e01c4a5652e0e5
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-10 12:45:51 +0200

    adding "Strict-Transport-Security" header for all non-error responses

--- src/controllers/responses/sendResponse200.ts
+++ src/controllers/responses/sendResponse200.ts
@@ -3,6 +3,7 @@ import express from "express"
 export function sendResponse200(data: unknown, res: express.Response) {
     res.set('Content-Type', 'text/html; charset=utf-8')
     res.set('Content-Security-Policy', "default-src 'self'; img-src 'self' data: upload.wikimedia.org live.staticflickr.com i.ytimg.com; frame-src www.youtube-nocookie.com")
+    res.set('Strict-Transport-Security', "max-age=63072000")
     res.set('Cache-Control', 'public, max-age=600, stale-while-revalidate=60')
     res.send(data)
 }
