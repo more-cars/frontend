c6e353a86008ee7403c75d3fbc2d4cee28b46038
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-10 15:26:41 +0200

    updating test step

--- tests/behavior/step_implementation/Then/the intro section should contain a link to {target}.ts
+++ tests/behavior/step_implementation/Then/the intro section should contain a link to {target}.ts
@@ -14,6 +14,10 @@ Then('the about section should contain a link to {string}', (target: string) => 
             cy.get(`footer nav[aria-label="Social Media Links"] [aria-label="More Cars videos at YouTube"]`)
                 .should('have.attr', 'href', 'https://www.youtube.com/@more-cars')
             break
+        case 'instagram':
+            cy.get(`footer nav[aria-label="Social Media Links"] [aria-label="More Cars photos at Instagram"]`)
+                .should('have.attr', 'href', 'https://www.instagram.com/more_cars_net/')
+            break
         default:
             assert.fail(`"${target}" is not a valid target`)
     }
