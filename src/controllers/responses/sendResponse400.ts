3a98684411dee6cc0ccaa5227c266a04851be0f4
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-14 10:08:03 +0200

    extending the header setting to not only cover the 200 responses, but also the 400 and 404 responses

--- src/controllers/responses/sendResponse400.ts
+++ src/controllers/responses/sendResponse400.ts
@@ -4,7 +4,13 @@ export function sendResponse400(res: express.Response) {
     return res.render('templates/nodes/invalid-request-page', {
         page_title: `Invalid request`
     }, (error, html) => {
-        res.statusCode = 400
+        res.status(400)
+        res.set('Content-Type', 'text/html; charset=utf-8')
+        res.set('Content-Security-Policy', "default-src 'self'; img-src 'self' data: upload.wikimedia.org live.staticflickr.com i.ytimg.com; frame-src www.youtube-nocookie.com")
+        res.set('Strict-Transport-Security', 'max-age=63072000')
+        res.set('X-Content-Type-Options', 'nosniff')
+        res.set('X-Frame-Options', 'DENY')
+        res.set('Cache-Control', 'public, max-age=30, stale-while-revalidate=60, stale-if-error=600')
         res.send(html)
     })
 }
--- src/controllers/responses/sendResponse404.ts
+++ src/controllers/responses/sendResponse404.ts
@@ -4,7 +4,13 @@ export function sendResponse404(res: express.Response) {
     return res.render('templates/nodes/node-not-found-page', {
         page_title: `Requested information not found`
     }, (error, html) => {
-        res.statusCode = 404
+        res.status(404)
+        res.set('Content-Type', 'text/html; charset=utf-8')
+        res.set('Content-Security-Policy', "default-src 'self'; img-src 'self' data: upload.wikimedia.org live.staticflickr.com i.ytimg.com; frame-src www.youtube-nocookie.com")
+        res.set('Strict-Transport-Security', 'max-age=63072000')
+        res.set('X-Content-Type-Options', 'nosniff')
+        res.set('X-Frame-Options', 'DENY')
+        res.set('Cache-Control', 'public, max-age=30, stale-while-revalidate=60, stale-if-error=600')
         res.send(html)
     })
 }
