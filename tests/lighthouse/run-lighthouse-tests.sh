84575b6967f918fe078965056ad80a2c6c6a8529
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-14 11:06:02 +0200

    adding "lighthouse" tests

---
+++ tests/lighthouse/run-lighthouse-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+ENV_FILE_PATH="$SCRIPT_PATH"/../../.env
+REPORTS_PATH="$SCRIPT_PATH"/../../test-reports/lighthouse
+
+source "$ENV_FILE_PATH"
+
+mkdir -p "$REPORTS_PATH"
+npx lighthouse "$FRONTEND_URL" --output html --output json --output-path "$REPORTS_PATH"/start-page-mobile
+npx lighthouse "$FRONTEND_URL" --output html --output json --output-path "$REPORTS_PATH"/start-page-desktop --preset=desktop
