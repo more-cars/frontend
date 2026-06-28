94d1951f90f340f7ea68ce659da1344cfd1e6125
Dennis Gerike <dennis.gerike+github@gmail.com>
2026-06-28 13:30:43 +0200

    switching from "kubernetes dashboard" to "headlamp"

--- deployment/minikube-cluster-start.sh
+++ deployment/minikube-cluster-start.sh
@@ -2,4 +2,5 @@
 
 minikube start --memory 8000 --cpus 2 -p morecars
 minikube addons enable metrics-server -p morecars
-minikube dashboard -p morecars
+minikube addons enable headlamp -p morecars
+minikube service headlamp -n headlamp -p morecars
